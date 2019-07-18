aad22717585f7f8d9f21e53a1939694e9d4e8766
DramaneD <ddramane63@gmail.com>
2019-07-18 16:28:43 +0000

    Added husky and precommit hooks

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World.</h1>
-        <img src="./logo.png" alt="company logo" />
         <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
