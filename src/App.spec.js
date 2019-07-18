c0d694d6ea433d8d9e2accf864c1987cc50b7dda
DramaneD <ddramane63@gmail.com>
2019-07-18 14:30:00 +0000

    Setup tests

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import '@testing-library/react/cleanup-after-each';
 import App from './App';
 
 describe('App', () => {
